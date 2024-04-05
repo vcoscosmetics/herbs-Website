0cc480dfb0c1038c1664acae37590e76821c7f3f
vcoscosmetics <vcoscosmetics@gmail.com>
2024-04-05 11:49:50 +0530

    added page loader

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -21,3 +21,17 @@ links.forEach((link) => {
 
 
 
+
+const loader = document.querySelector("#preloader");
+window.addEventListener('load', () =>{
+  setTimeout(()=>{
+    
+      loader.style.display = "none";
+
+  },2000)
+})    
+
+
+
+
+
